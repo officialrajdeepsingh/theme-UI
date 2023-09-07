0b014be67f7ec0602c60568d11aae9b99cd1efee
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-07 23:16:56 +0530

    rename the ThmeUIProvider to ThemeProvider

--- src/app/ThemeUIProvider.tsx
+++
@@ -1,9 +0,0 @@
-/** @jsxImportSource theme-ui */
-import { ThemeUIProvider } from "theme-ui";
-import { theme } from "./theme";
-import * as React from "react";
-export const App = ({ children }: { children: React.ReactNode }) => (
-  <ThemeUIProvider theme={theme}>
-    {children}
-  </ThemeUIProvider>
-);
