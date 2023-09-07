e3f0db84984b35519e1ca4cdf7cc918d3f53695b
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-07 23:15:06 +0530

    example theme

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,14 +1,54 @@
-import type { Theme } from 'theme-ui'
+import { makeTheme } from '@theme-ui/css/utils'
 
-export const theme: Theme = {
-  fonts: {
-    body: 'system-ui, sans-serif',
-    heading: '"Avenir Next", sans-serif',
-    monospace: 'Menlo, monospace',
+export const theme = makeTheme({
+
+  config: {
+    initialColorModeName: 'light',
+    useColorSchemeMediaQuery: true,
   },
+
   colors: {
     text: '#000',
     background: '#fff',
-    primary: '#33e',
+    primary: '#07c',
+    secondary: '#b0b',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#222',
+        primary: '#0cf',
+        secondary: '#faf',
+      },
+    },
   },
-}
+
+  fonts: {
+    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'sans-serif',
+  },
+  radii: [0, 4],
+
+  styles: {
+    root: {
+      fontFamily: 'body',
+      color: 'text',
+      bg: 'background',
+      p: 4,
+    },
+    a: {
+      color: 'primary',
+      fontWeight: 'bold',
+      textDecoration: 'none',
+      ':hover': {
+        color: 'secondary',
+        textDecoration: 'underline',
+      },
+    },
+  },
+
+  buttons: {
+    primary: {
+      cursor: 'pointer',
+    },
+  },
+})
