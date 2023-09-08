3146f3ce9441f9e0e470e4fac930ba90343fde55
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-08 14:06:10 +0530

    update the example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 "use client";
-import { Box, Button, Flex, Heading } from "theme-ui";
+import { Box, Button, Flex, Heading, Container, Alert, Close } from "theme-ui";
 import { Toggle } from "@/components/Toggle";
 export default function Home() {
   return (
-    <Box p={4} bg="highlight">
-      <Flex
-        sx={{
-          alignItems: "center",
-        }}
-      >
-        <Heading>Components</Heading>
-        <Button ml="auto">Beep</Button>
-        <Toggle />
-      </Flex>
-    </Box>
+    <>
+      <Container p={4} bg="muted">
+        <Box p={4} bg="highlight">
+          <Flex
+            sx={{
+              alignItems: "center",
+            }}
+          >
+            <Heading>Components</Heading>
+            <Button ml="auto">Beep</Button>
+            <Toggle />
+          </Flex>
+        </Box>
+      </Container>
+      
+      <Container mt={"10px"} p={"10px"} >
+        <Alert>
+          Beep boop, this is an alert!
+          <Close ml="auto" mr={-2} />
+        </Alert>
+      </Container>
+
+    </>
   );
 }
