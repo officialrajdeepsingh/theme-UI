eab3fde62843d58294225b32c730ba4976b5a103
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-09 19:57:41 +0530

    theme provider

--- src/app/ThemeProvider.tsx
+++ src/app/ThemeProvider.tsx
@@ -1,10 +1,22 @@
-"use client"
+"use client";
 /** @jsxImportSource theme-ui */
 import { ThemeUIProvider } from "theme-ui";
 import { theme } from "./theme";
 import * as React from "react";
+import { Header } from "@/components/Header";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+/**
+ * @see https://emotion.sh/docs/@emotion/cache
+ */
+const key = "custom";
+const cache = createCache({ key });
+// const cache = createCache()
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => (
-  <ThemeUIProvider theme={theme}>
-    {children}
-  </ThemeUIProvider>
+  <CacheProvider value={cache}>
+    <ThemeUIProvider theme={theme}>
+      <Header />
+      {children}
+    </ThemeUIProvider>
+  </CacheProvider>
 );
